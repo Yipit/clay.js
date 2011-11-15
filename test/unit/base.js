37035ed4d1c07f9b4fbd7b21da11c0699bcda933
Gabriel Falcao <gabriel@nacaolivre.org>
2011-11-15 18:11:06 -0200

    Array.prototype.models getter

--- test/unit/base.js
+++ test/unit/base.js
@@ -23,25 +23,45 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE. */
 var vows = require('vows')
-, assert = require('assert');
+, should = require('should');
 
-require('clay');
+var models = require('clay');
 
 vows.describe('Monkey-patches/Additions to the native prototypes').addBatch({
     'String.slugify': function() {
-        assert.equal("gabriel-falcao".slugify(), "gabriel-falcao");
+        should.equal("gabriel-falcao".slugify(), "gabriel-falcao");
     },
     'String.methodify': function() {
-        assert.equal("Call Gabriel Falcão".methodify(), "call_gabriel_falcao");
+        should.equal("Call Gabriel Falcão".methodify(), "call_gabriel_falcao");
     },
     'String.render': function() {
-        assert.equal("Gabriel {surname}".render({surname: 'Falcão'}), "Gabriel Falcão");
+        should.equal("Gabriel {surname}".render({surname: 'Falcão'}), "Gabriel Falcão");
     },
     'Array.prototype.first always return the first element': function() {
-        assert.equal(["aaa", "b"].first, "aaa");
+        should.equal(["aaa", "b"].first, "aaa");
     },
     'Array.prototype.last always return the last element': function() {
-        assert.equal(["aaa", "b"].last, "b");
+        should.equal(["aaa", "b"].last, "b");
+    },
+    'Array.prototype.models return unique models': function() {
+        var Person = models.declare('Person');
+        var p1 = new Person();p1.__id__ = 1;
+        var p2 = new Person();p2.__id__ = 2;
+        var p3 = new Person();p3.__id__ = 3;
+
+        var p4 = new Person();p4.__data__.__id__ = 1;
+        var p5 = new Person();p5.__data__.__id__ = 2;
+        var p6 = new Person();p6.__data__.__id__ = 3;
+
+        var People = [];
+        People.push(p1);
+        People.push(p2);
+        People.push(p3);
+        People.push(p4);
+        People.push(p5);
+        People.push(p6);
+
+        People.models.should.eql([p1, p2, p3]);
     },
     'Number.prototype.times takes a callback': {
         'the first argument is the last captured error': function(){
@@ -56,26 +76,26 @@ vows.describe('Monkey-patches/Additions to the native prototypes').addBatch({
                 }
             });
 
-            assert.equal(x, 2);
-            assert.equal(errors.length, 1);
-            assert.equal(errors[0].message, "It has failed");
+            should.equal(x, 2);
+            should.equal(errors.length, 1);
+            should.equal(errors[0].message, "It has failed");
         },
         'the second argument is the index': function(){
             var expected_indexes = [0, 1, 2, 3];
 
-            assert.deepEqual((4).times(function(e, index){
-                assert.equal(e, null);
-                assert.include(expected_indexes, index);
-                assert.equal(index, expected_indexes[index]);
+            should.deepEqual((4).times(function(e, index){
+                should.equal(e, null);
+                should.include(expected_indexes, index);
+                should.equal(index, expected_indexes[index]);
                 return index;
             }), expected_indexes);
         },
         'the third argument is the number of remaining executions': function(){
             var expected_remaining = [3, 2, 1, 0];
 
-            assert.deepEqual((4).times(function(e, index, remaining){
-                assert.ifError(e);
-                assert.equal(remaining, expected_remaining[index]);
+            should.deepEqual((4).times(function(e, index, remaining){
+                should.ifError(e);
+                should.equal(remaining, expected_remaining[index]);
                 return remaining;
             }), expected_remaining);
         },
@@ -93,8 +113,8 @@ vows.describe('Monkey-patches/Additions to the native prototypes').addBatch({
                 }
             ],
             'and the first occurrence responds properly': function(expected){
-                assert.deepEqual((3).times(function(e, index, remaining, this_time) {
-                    assert.deepEqual(expected[index], this_time);
+                should.deepEqual((3).times(function(e, index, remaining, this_time) {
+                    should.deepEqual(expected[index], this_time);
                     return this_time;
                 }), expected);
             }
